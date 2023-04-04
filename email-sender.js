0e4020c580f1fe52279313d28878c6e19023fa11
ezitounioussama <zitounioussama95@gmail.com>
2023-04-04 20:30:41 +0000

    improving all files with comments

--- email-sender.js
+++ email-sender.js
@@ -1,24 +1,27 @@
 const nodemailer = require("nodemailer");
-
+// create a transport object with the SMTP configuration
 const transporter = nodemailer.createTransport({
+  // specify the service used for sending email (Gmail in this case)
   service: "gmail",
   auth: {
+    // specify the email address and the application-specific password for authentication
     user: "zitounioussama95@gmail.com",
     pass: "it works with the password I generate it in gmail but I can't leave it here for security ahhaahahahahah",
   },
 });
-
+// specify the email content
 const mailOptions = {
   from: "zitounioussama95@gmail.com",
   to: "ezitounioussama@gmail.com",
   subject: "Sending Email using Node.js",
   text: "That was easy!",
 };
-
+// send the email using the transport object
 transporter.sendMail(mailOptions, function (error, info) {
   if (error) {
     console.log(error);
   } else {
+    // if the email is sent successfully, log the response to the console
     console.log("Email sent: " + info.response);
   }
 });
--- password-generator.js
+++ password-generator.js
@@ -1,11 +1,17 @@
+// Import the generate-password library
 const generatePassword = require("generate-password");
 
+// Define a function to generate a random password
 function generateRandomPassword() {
+  // Generate a password with a length of 10 characters and including numbers
   const password = generatePassword.generate({
     length: 10,
     numbers: true,
   });
+
+  // Log the generated password to the console
   console.log(`Your randomly generated password is: ${password}`);
 }
 
+// Call the function to generate a random password
 generateRandomPassword();
